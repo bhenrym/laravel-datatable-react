d2b78519f31dbbdb3660cdd7a18c412e70091ad7
Yottabm <yottabm@gmail.com>
2021-08-20 16:54:24 -0400

    fixed security records

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,92 +15,92 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
 
- 
+
     public function run()
     {
 
         //$this->call(CategoriaSeeder::class);
-        
+
 
         DB::table('people')->insert([
             'id'=> 1,
-            'type_doc'=>'Ci', 
-            'number_doc'=>'11070630', 
-            'first_name'=>'Henry', 
-            'second_name'=>'Jhonny', 
+            'type_doc'=>'Ci',
+            'number_doc'=>'11070630',
+            'first_name'=>'Henry',
+            'second_name'=>'Jhonny',
             'ap_paternal'=>'Bautista',
             'ap_maternal'=>'Mamani',
-            'address'=>'Av. juan pablo II calle jose arabe', 
+            'address'=>'Av. juan pablo II calle jose arabe',
             'contact'=>'77702824'
         ]);
 
         DB::table('people')->insert([
             'id'=> 2,
-            'type_doc'=>'Ci', 
-            'number_doc'=>'123456789', 
-            'first_name'=>'Julio', 
-            'second_name'=>'Eduardo', 
+            'type_doc'=>'Ci',
+            'number_doc'=>'123456789',
+            'first_name'=>'Julio',
+            'second_name'=>'Eduardo',
             'ap_paternal'=>'Paredes',
             'ap_maternal'=>'Ramos',
-            'address'=>'Calle extranca los montes', 
+            'address'=>'Calle extranca los montes',
             'contact'=>'77262115'
         ]);
 
 
         DB::table('people')->insert([
             'id'=> 3,
-            'type_doc'=>'Ci', 
-            'number_doc'=>'6046805', 
-            'first_name'=>'William', 
-            'second_name'=>'Henry', 
+            'type_doc'=>'Ci',
+            'number_doc'=>'6046805',
+            'first_name'=>'William',
+            'second_name'=>'Henry',
             'ap_paternal'=>'Bill',
             'ap_maternal'=>'Gates',
-            'address'=>'Washington', 
+            'address'=>'Washington',
             'contact'=>'247262115'
         ]);
 
         DB::table('people')->insert([
             'id'=> 4,
-            'type_doc'=>'Ci', 
-            'number_doc'=>'123123454', 
-            'first_name'=>'Evan', 
-            'second_name'=>'', 
+            'type_doc'=>'Ci',
+            'number_doc'=>'123123454',
+            'first_name'=>'Evan',
+            'second_name'=>'',
             'ap_paternal'=>'You',
             'ap_maternal'=>'Nat',
-            'address'=>'Livingston, New Jersey', 
+            'address'=>'Livingston, New Jersey',
             'contact'=>'77262115'
         ]);
 
         DB::table('people')->insert([
             'id'=> 5,
-            'type_doc'=>'Nit', 
-            'number_doc'=>'11788451', 
-            'first_name'=>'Elon', 
-            'second_name'=>'Reeve', 
+            'type_doc'=>'Nit',
+            'number_doc'=>'11788451',
+            'first_name'=>'Elon',
+            'second_name'=>'Reeve',
             'ap_paternal'=>'Mosk',
             'ap_maternal'=>'Maye',
-            'address'=>'Estado de florida', 
+            'address'=>'Estado de florida',
             'contact'=>'22587445'
         ]);
 
         DB::table('people')->insert([
             'id'=> 6,
-            'type_doc'=>'Pass', 
-            'number_doc'=>'123123454', 
-            'first_name'=>'Luis', 
-            'second_name'=>'Julian', 
+            'type_doc'=>'Pass',
+            'number_doc'=>'123123454',
+            'first_name'=>'Luis',
+            'second_name'=>'Julian',
             'ap_paternal'=>'Bautista',
             'ap_maternal'=>'Romero',
-            'address'=>'Calle copacabana', 
+            'address'=>'Calle copacabana',
             'contact'=>'77702569'
         ]);
 
-        
+
 
         //******************************************************************** */
- 
 
-   
+
+
         //Entidad::create();  // no funciona por convencion de laravel result=entidads
 
         //********************************************************************** */
@@ -123,7 +123,7 @@ class DatabaseSeeder extends Seeder
             'id' => 1,
             'name_user' => 'Henry',
             'email' => 'henryingsis61@gmail.com',
-            'password' => Hash::make('beautiful1996'),
+            'password' => Hash::make('123456789'),
             'person_id' => 1,
             'role_id' => 1,
         ]);
@@ -132,7 +132,7 @@ class DatabaseSeeder extends Seeder
             'id' => 2,
             'name_user' => 'Julio',
             'email' => 'pipoangelitos@gmail.com',
-            'password' => Hash::make('600513001pipo'),
+            'password' => Hash::make('123456789'),
             'person_id' => 2,
             'role_id' => 2,
         ]);
@@ -141,7 +141,7 @@ class DatabaseSeeder extends Seeder
             'id' => 3,
             'name_user' => 'William',
             'email' => 'william@gmail.com',
-            'password' => Hash::make('beautiful1996'),
+            'password' => Hash::make('123456789'),
             'person_id' => 3,
             'role_id' => 2,
         ]);
@@ -150,7 +150,7 @@ class DatabaseSeeder extends Seeder
             'id' => 4,
             'name_user' => 'Evan',
             'email' => 'evan_you@gmail.com',
-            'password' => Hash::make('beautiful1996'),
+            'password' => Hash::make('123456789'),
             'person_id' => 4,
             'role_id' => 2,
         ]);
@@ -159,7 +159,7 @@ class DatabaseSeeder extends Seeder
             'id' => 5,
             'name_user' => 'Elon',
             'email' => 'elo_mosk@gmail.com',
-            'password' => Hash::make('beautiful1996'),
+            'password' => Hash::make('123456789'),
             'person_id' => 5,
             'role_id' => 2,
         ]);
@@ -168,18 +168,18 @@ class DatabaseSeeder extends Seeder
             'id' => 6,
             'name_user' => 'Luis',
             'email' => 'julianingsis61@gmail.com',
-            'password' => Hash::make('beautiful1996'),
+            'password' => Hash::make('123456789'),
             'person_id' => 6,
             'role_id' => 1,
         ]);
 
 
         //********************************************************************** */
-        
-        
 
 
-        
+
+
+
 
 
     }
